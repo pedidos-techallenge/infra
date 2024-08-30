9eaa824e175e9edcc8dc80bd0fa2ce9ae1714174
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-08-30 10:43:52 -0300

    Add Terraform for Bucket S3

---
+++ bucket/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "bucket-tfstates-postech-fiap-6soat"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "bucket-tfstates"
+  }
+}
