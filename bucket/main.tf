3228756c594105b7d2a5cac03e9003169bc3f50a
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-08-31 14:21:30 -0300

    Remove calling for access key and key id from bucket main.tf

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region  = "us-east-1"
-  aws_access_key_id = secrets.AWS_ACCESS_KEY_ID
-  aws_secret_access_key = secrets.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_s3_bucket" "terraform_state" {
