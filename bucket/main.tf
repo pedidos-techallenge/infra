0e466e1468a15cee52dcec972e7c595990120542
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-11 21:06:19 -0300

    Add policy to grant access to put objects in s3 bucket

--- bucket/main.tf
+++ bucket/main.tf
@@ -13,3 +13,26 @@ resource "aws_s3_bucket" "terraform_state" {
     Name = "bucket-tfstates"
   }
 }
+
+resource "aws_iam_policy" "s3_put_policy" {
+  name        = "S3PutObjectPolicy"
+  description = "Permission to send objects to S3."
+
+  policy = jsonencode({
+    Version = "2024-09-11"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::bucket-tfstates-postech-fiap-6soat",
+          "arn:aws:s3:::bucket-tfstates-postech-fiap-6soat/*"
+        ]
+      }
+    ]
+  })
+}
