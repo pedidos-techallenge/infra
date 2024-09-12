38291cf4c9a5538de94395fd81d0e5d83d1af3ff
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-11 21:13:13 -0300

    Fix json sintax

--- bucket/main.tf
+++ bucket/main.tf
@@ -19,16 +19,16 @@ resource "aws_iam_policy" "s3_put_policy" {
   description = "Permission to send objects to S3."
 
   policy = jsonencode({
-    Version = "2024-09-11"
-    Statement = [
+    "Version" = "2024-09-11"
+    "Statement" = [
       {
-        Action = [
+        "Action" = [
           "s3:PutObject",
           "s3:GetObject",
           "s3:ListBucket"
         ]
-        Effect   = "Allow"
-        Resource = [
+        "Effect"   = "Allow"
+        "Resource" = [
           "arn:aws:s3:::bucket-tfstates-postech-fiap-6soat",
           "arn:aws:s3:::bucket-tfstates-postech-fiap-6soat/*"
         ]
