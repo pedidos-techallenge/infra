7e8902efa1ff4560569e304b471867aa4fbf89a6
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-23 17:15:28 -0300

    Change API name

--- network/main.tf
+++ network/main.tf
@@ -76,5 +76,5 @@ resource "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "application_resource" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "pedidosapi"
+  path_part   = "pedidos"
 }
