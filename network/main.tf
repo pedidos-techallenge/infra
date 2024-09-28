b7a78b576b5c55198cb625b1fbaf61914de240a7
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-28 12:11:46 -0300

    Add new available zones

--- network/main.tf
+++ network/main.tf
@@ -17,18 +17,23 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "private" {
+  count      = 2
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.${count.index}.0/24"
   map_public_ip_on_launch = false
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
 
   tags = {
     Name = "subnet-private"
   }
 }
 
+
 resource "aws_subnet" "public" {
+  count      = 2
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
