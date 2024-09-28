365cccdad50519c54707c8ba660d36670129b1e3
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-28 12:18:01 -0300

    Add index count in public vpc

--- network/main.tf
+++ network/main.tf
@@ -68,7 +68,8 @@ resource "aws_route" "public_route" {
 
 # Link the public subnet to route table
 resource "aws_route_table_association" "public_rt_association" {
-  subnet_id      = aws_subnet.public.id
+  count          = 2
+  subnet_id      = aws_subnet.public[couny.index].id
   route_table_id = aws_route_table.public_rt.id
 }
 
