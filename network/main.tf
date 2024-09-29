60ccf3a92a7f43d5decb31b869f9b281e54a66ab
ederpbrito <ederpsilva@hotmail.com>
2024-09-28 21:16:58 -0300

    correção erro

--- network/main.tf
+++ network/main.tf
@@ -71,7 +71,7 @@ resource "aws_route" "public_route" {
 
 # Link the public subnet to route table
 resource "aws_route_table_association" "public_rt_association" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = aws_subnet.public[*].ids
   route_table_id = aws_route_table.public_rt.id
 }
 
