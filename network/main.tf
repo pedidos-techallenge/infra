9894468c264d4fa5e6bbe23ca0f7a7c9e85eb436
ederpbrito <ederpsilva@hotmail.com>
2024-09-28 21:31:12 -0300

    correção subnets AZ

--- network/main.tf
+++ network/main.tf
@@ -71,7 +71,8 @@ resource "aws_route" "public_route" {
 
 # Link the public subnet to route table
 resource "aws_route_table_association" "public_rt_association" {
-  subnet_id      = aws_subnet.public[*].ids
+  count          = length(aws_subnet.public) 
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
 
