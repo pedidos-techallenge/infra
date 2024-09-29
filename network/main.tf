3fb45795c3af8e1c718bed0fee6892d8b147e699
ederpbrito <ederpsilva@hotmail.com>
2024-09-29 16:30:17 -0300

    adição de duas zonas de disponibilidade

--- network/main.tf
+++ network/main.tf
@@ -20,27 +20,47 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "private" {
-  count                   = 2
+resource "aws_subnet" "public_az1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "subnet-private"
-  }
-}
-
-resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "subnet-public"
+    Name = "subnet-public-az1"
+  }
+}
+
+resource "aws_subnet" "private_az1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "subnet-private-az1"
+  }
+}
+
+resource "aws_subnet" "public_az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet-public-az2"
+  }
+}
+
+resource "aws_subnet" "private_az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "subnet-private-az2"
   }
 }
 
@@ -69,10 +89,15 @@ resource "aws_route" "public_route" {
   gateway_id             = aws_internet_gateway.igw.id
 }
 
-# Link the public subnet to route table
-resource "aws_route_table_association" "public_rt_association" {
-  count          = length(aws_subnet.public) 
-  subnet_id      = aws_subnet.public[count.index].id
+# Associação da tabela de rotas com a subnet pública
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_az1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Associação da tabela de rotas com a subnet pública AZ2
+resource "aws_route_table_association" "public_association_az2" {
+  subnet_id      = aws_subnet.public_az2.id
   route_table_id = aws_route_table.public_rt.id
 }
 
