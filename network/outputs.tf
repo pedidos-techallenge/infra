7068b00d005c0c8c62de86e0605d621de5079190
jubel <jubelcassioverridasilva@gmail.com>
2024-10-01 00:07:47 -0300

    Fixing tf outputs

--- network/outputs.tf
+++ network/outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnets" {
-  value = [aws_subnet.public_az1.id, aws_subnet.public_az2.id]
+  value = module.vpc.public_subnets
 }
 
 output "private_subnets" {
-  value = [aws_subnet.private_az1.id, aws_subnet.private_az2.id]
+  value = module.vpc.private_subnets
 }
